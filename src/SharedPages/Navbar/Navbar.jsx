3738f37ed1c54b05839fa25dbd808c3b59ddedf8
FreelancerTasin507 <freelancertasin507@gmail.com>
2024-06-15 22:19:57 +0600

    navbar created

--- src/SharedPages/Navbar/Navbar.jsx
+++ src/SharedPages/Navbar/Navbar.jsx
@@ -62,11 +62,11 @@ import {
                   className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
                 >
                   <li>
-                    <a>Item 1</a>
+                    <a>Home</a>
                   </li>
-                  <li>
-                    <a>Parent</a>
-                    <ul className="p-2">
+                  <li className="">
+                    <a>Catelogue</a>
+                    <ul className="p-2 w-full absolute">
                       <li>
                         <a>Submenu 1</a>
                       </li>
