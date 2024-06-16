175ae60afc4c392799c06c06cbc44c0b62062b5e
FreelancerTasin507 <freelancertasin507@gmail.com>
2024-06-16 15:04:06 +0600

    fake product added

--- src/Pages/Home/What_new/WhatNew.jsx
+++ src/Pages/Home/What_new/WhatNew.jsx
@@ -50,7 +50,7 @@ const WhatNew = () => {
             </figure>
             <div className="card-body">
               <h2 className="card-title">Life hack</h2>
-              <p>How to park your car at your garage?</p>
+              <p>How to park your car --at your garage?</p>
               <div className="card-actions justify-end">
                 <button className="btn btn-accent">Add to Cart</button>
               </div>
